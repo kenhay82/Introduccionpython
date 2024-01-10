36a931856bed2d576b45804d6668348f0b62bad6
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-09 20:38:39 -0500

    nt1 se implementa algoritmo de datos primitivos y suma desde consola

---
+++ datos.py
@@ -0,0 +1,9 @@
+numeroUno=int(input("Digita un número entero: "))
+
+numeroDos=int(input("Digita un número entero: "))
+
+#Cómo hacer con un Float? 
+
+sumatoria = numeroUno + numeroDos
+
+print(f"La suma de: {numeroUno} + {numeroDos} es = {sumatoria}")
---
+++ main.py
@@ -0,0 +1,13 @@
+#Variables primitivas con Python
+#Quemar las variables - Asignarles valor
+nombreEstudiante = "Carlos Mario"
+edadEstudiante = 29
+estaturaEstudiante = 1.824
+hinchaDelMejorEquipo = False
+
+#Pedir datos por teclado
+comidaFavorita = input("Digita tu comida favorita: ")
+
+#Mostrar datos en pantalla (Salida)
+print(f"Hola: {nombreEstudiante}, tu comida favorita es {comidaFavorita}")
+
